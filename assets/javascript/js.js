f2b95860fbd546c88d57f13b058ec5b7e9d33b05
unknown <sjirjis@gmail.com>
2017-09-09 19:50:03 -0700

    built & styled timer

---
+++ assets/javascript/js.js
@@ -0,0 +1,28 @@
+$('#timer').hide();
+
+$('#start').on('click', function() {
+  $('#start').hide();
+  $('#timer').show();
+
+  var quizDuration = 3
+  $('#timerText').html(quizDuration);
+
+  var quizExpire = new Date();
+  quizExpire.setSeconds(quizExpire.getSeconds() + quizDuration);
+
+  var timer = setInterval(function() {
+    var now = new Date();
+    var timeLeft = Math.ceil((quizExpire - now) / 1000);
+
+    $('#timerText').html(timeLeft);
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      $('#timerText').css({
+        'background-color': '#C0392B',
+        'color': '#fff'
+      });
+      $('#timerText').html("Time's Up!");
+    }
+  }, 1000);
+});
